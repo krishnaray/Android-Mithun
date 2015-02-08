0ef30495c68264230d1ed4d7c20cb8d7ce81174d
krishna.ray <krishna.ray.eee@gmail.com>
2015-02-08 14:24:01 -0800

    Proximity App Added Signed-off-by: krishna.ray <krishna.ray.eee@gmail.com>

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/5f/20b266e1dbaf00141f7fd6a025c91623
@@ -0,0 +1,5 @@
+package com.lict.shakeditectapp.sensors.utils;
+
+public interface OnShakeListener {
+	public void onShake(int count);
+}
---
+++ ProximityApp/src/com/lict/proximityapp/sensors/utils/OnProximityChangeListener.java
@@ -0,0 +1,5 @@
+package com.lict.proximityapp.sensors.utils;
+
+public interface OnProximityChangeListener {
+	public void onChange(float distanceInCentimeters);
+}
